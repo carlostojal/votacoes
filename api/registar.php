9c581099c67a63f146233c6cc4f354314493ca2f
Carlos Tojal <carlos.tojal@hotmail.com>
2020-10-20 23:16:17 +0100

    started preparing for email send

--- api/registar.php
+++ api/registar.php
@@ -1,5 +1,9 @@
 <?php
 
+  require("../libs/phpmailer/src/Exception.php");
+  require("../libs/phpmailer/src/PHPMailer.php");
+  require("../libs/phpmailer/src/SMTP.php");
+
   if(!$_POST["email"]) {
     echo "EMAIL_NOT_PROVIDED";
     exit();
@@ -10,21 +14,23 @@
 
   require("./connection.php");
 
-  $sql = "SELECT id FROM Boletim WHERE email = ?";
+  $sql = "SELECT id, usado FROM Boletim WHERE email = ?";
   $stm = $conn->prepare($sql);
   $stm->bind_param("s", $email);
   $stm->execute();
 
   $result = $stm->get_result();
   if($result->num_rows > 0) {
-    echo "EMAIL_ALREADY_USED";
-    exit();
+    // se foi usado, retorna erro e sai
+    // senão, reenvia código do boletim
   }
 
   $sql = "INSERT INTO Boletim (id, email) VALUES (?, ?)";
   $stm = $conn->prepare($sql);
   $stm->bind_param("is", $id, $email);
   $stm->execute();
+
+  // envia código do boletim
 
   $stm->close();
   $conn->close();
