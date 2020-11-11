4a060a701a37f65d2913d7aedffca7bfb2d03a2f
Carlos Tojal <carlos.tojal@hotmail.com>
2020-11-11 20:06:33 +0000

    about page update

--- votacoes/src/Components/Screens/Sobre.js
+++ votacoes/src/Components/Screens/Sobre.js
@@ -3,16 +3,32 @@ import Template from "../Misc/Template";
 export default function Sobre() {
   return (
     <Template title="Sobre">
-      <h3>Tecnologias usadas:</h3>
+      <p>Tal como no voto tradicional, o mesmo é anónimo.</p>
+      <p>
+        A cada endereço de email institucional é associado um nº de boletim que
+        contém um código de confirmação para assegurar que cada aluno não
+        consiga utilizar o boletim de outro. O endereço de email encontra-se
+        devidamente encriptado na base de dados.
+      </p>
+      <p>
+        Ao votar, é registado na base de dados o voto e a respetiva data e hora
+        sem qualquer associação ao boletim.
+        O boletim é inutilizado.
+      </p>
+      <p>
+        Assim, cada boletim só tem uma utilização válida, e não é possível
+        saber por quem foram registados os votos.
+      </p>
+      <h3>Tecnologias usadas</h3>
       <br></br>
-      <h4>Frontend:</h4>
+      <h4>Frontend</h4>
       <ul>
         <li>ReactJS</li>
         <li>React Bootstrap</li>
         <li>AlertifyJS</li>
         <li>Chart.js</li>
       </ul>
-      <h4>Backend:</h4>
+      <h4>Backend</h4>
       <ul>
         <li>PHP</li>
         <li>MySQL</li>
